49e923744aab38ce88f2fc446682328dab7ee633
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2021-12-28 18:36:31 +0300

    `SpinLockUtilNS` Updated `SpinLockUtil`

--- SpinLockUtilNS/SpinLockUtil.cs
+++ SpinLockUtilNS/SpinLockUtil.cs
@@ -9,7 +9,7 @@ public static class SpinLockUtil
     public const int False = 0;
     public const int True = 1;
 
-    public static void AcquireLock(this ref int isLocked)
+    public static void AcquireLock(ref int isLocked)
     {
         while (true)
             if (TryLock(isLocked: ref isLocked))
@@ -20,7 +20,7 @@ public static class SpinLockUtil
     ///     According to https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/variables#atomicity-of-variable-references
     ///     reads of `int`-s are atomic
     /// </summary>
-    public static bool IsLockedOnce(this ref int isLocked)
+    public static bool IsLockedOnce(ref int isLocked)
     {
         return isLocked == True;
     }
@@ -29,12 +29,12 @@ public static class SpinLockUtil
     ///     According to https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/variables#atomicity-of-variable-references
     ///     reads of `int`-s are atomic
     /// </summary>
-    public static bool IsUnlocked(this ref int isLocked)
+    public static bool IsUnlocked(ref int isLocked)
     {
         return isLocked == False;
     }
 
-    public static bool TryLock(this ref int isLocked)
+    public static bool TryLock(ref int isLocked)
     {
         return Interlocked.CompareExchange(
             location1: ref isLocked,
@@ -43,7 +43,7 @@ public static class SpinLockUtil
         ) == False;
     }
 
-    public static void UnlockOne(this ref int isLocked)
+    public static void UnlockOne(ref int isLocked)
     {
         Interlocked.Decrement(location: ref isLocked);
     }
